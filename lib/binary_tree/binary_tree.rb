0fdd06617fac3f01397b87ea93d384ab3bae93ca
hugorut <hugorut@gmail.com>
2015-12-26 18:53:22 +0000

    medn

--- lib/binary_tree/binary_tree.rb
+++ lib/binary_tree/binary_tree.rb
@@ -1,17 +1,59 @@
 module BinaryTree
     class Tree
+        attr_reader :root
         
-        
+        def initialize
+            @root = nil    
+        end
+
+        # add a node to the tree by travesing down either left or right side
+        # until the appropriate value is found
+        def add_node(key, value)
+            node = Node.new(key, value)
+
+            # if there is current no root node set for the tree we need to set it
+            if root.nil?
+                @root = node
+            else
+                # lets find where we need to place the node 
+                current = @root
+                placed = false
+
+                while not placed
+                    # if the current key is less than the current key we need to select the 
+                    # left hand track to traverse down
+                    if current.key > node.key
+                        property = 'left_child'
+                    else
+                        # we travese down the right hand side
+                        property = 'right_child'
+                    end
+
+                    # set a pointer to remember the previous loops node value and
+                    # change the current value current based on the property flag 
+                    parent = current
+                    current = current.send(property)
+
+                    # if the node under examination is nil then this means we have found
+                    # a spot to place the current node and we set the placed flag to 
+                    # true in order to exit out of the loop
+                    if current.nil?
+                        parent.send(property+'=', node)
+                        placed = true
+                    end
+                end
+
+            end
+        end
     end
 
     class Node
         attr_accessor :left_child, :right_child
-        attr_reader :index, :value
+        attr_reader :key, :value
 
-        def initialize(index, value)
-            @index = index
+        def initialize(key, value)
+            @key = key
             @value = value    
         end
-        
     end
 end
--- test/binary_tree/test_binary_tree.rb
+++ test/binary_tree/test_binary_tree.rb
@@ -1,14 +1,24 @@
 require "minitest/autorun"
+require "yaml"
 require "./lib/binary_tree/binary_tree.rb"
 
 class TestBinaryTree < Minitest::Test
     def setup
         @tree = BinaryTree::Tree.new
+        @fixtures = YAML.load_file(File.dirname(__FILE__)+"/tree_items.yaml")
     end
     
     def teardown
         @tree = nil
+        @fixtures = nil
     end    
+
+    def get_fixture(index = nil)
+        fixture = (index) ? @fixtures[index] : @fixtures.sample
+        @fixtures.delete(fixture)
+
+        return fixture
+    end
 
     def test_node_has_appropriate_left_right_properties
         node = BinaryTree::Node.new(50, 'test')
@@ -20,5 +30,21 @@ class TestBinaryTree < Minitest::Test
 
         assert_equal(right, node.right_child)
         assert_equal(left, node.left_child)
+    end
+
+    def test_node_is_added_to_tree
+        fixture = get_fixture
+
+        @tree.add_node(fixture['key'], fixture['value'])
+        assert_equal(fixture['key'], @tree.root.key)
+        
+        second_fixture = get_fixture
+        @tree.add_node(second_fixture['key'], second_fixture['value'])
+
+        if second_fixture['key'] > fixture['key']
+            assert_equal(second_fixture['key'], @tree.root.right_child.key)
+        else
+            assert_equal(second_fixture['key'], @tree.root.left_child.key)
+        end
     end
 end
