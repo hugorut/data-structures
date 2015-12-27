e1a9dcac8bccd1df3270a9d2164830678a4457ef
hugorut <hugorut@gmail.com>
2015-12-27 10:33:18 +0000

    find and traverse in order

--- lib/binary_tree/binary_tree.rb
+++ lib/binary_tree/binary_tree.rb
@@ -1,7 +1,7 @@
 module BinaryTree
     class Tree
         attr_reader :root
-        
+
         def initialize
             @root = nil    
         end
@@ -20,14 +20,8 @@ module BinaryTree
                 placed = false
 
                 while not placed
-                    # if the current key is less than the current key we need to select the 
-                    # left hand track to traverse down
-                    if current.key > node.key
-                        property = 'left_child'
-                    else
-                        # we travese down the right hand side
-                        property = 'right_child'
-                    end
+                    # get the property to travese, either left_child or right_child
+                    property = direction(node.key, current.key)
 
                     # set a pointer to remember the previous loops node value and
                     # change the current value current based on the property flag 
@@ -44,6 +38,50 @@ module BinaryTree
                 end
 
             end
+        end
+
+        # find the direction to placed the node using a comparison against the node to be placed
+        # and the current node under inspection
+        def direction(find_index, current_index)
+            # if the current key is less than the current key we need to select the 
+            # left hand track to traverse down
+            if current_index > find_index
+                return 'left_child'
+            else
+                # we travese down the right hand side
+                return 'right_child'
+            end
+        end
+
+        # find a node in the tree using a index key
+        def find(index)
+            current = @root
+
+            while current.key != index
+                # get the property to travese, either left_child or right_child
+                # then set current to equal that traversal
+                property = direction(index, current.key)
+                current = current.send(property)
+
+                # if the next node is nil then we need to exit out the loop as the index
+                # does not exist in the tree
+                if current.nil?
+                    return false
+                end
+            end
+
+            # we've found the node! return it
+            return current
+        end
+
+        # traverse the tree in order of accending value and yield
+        # a block with the node as a parameter if given
+        def travese_in_order(node = @root)
+            return if node == nil
+            
+            travese_in_order(node.left_child)
+            yield node if block_given?
+            travese_in_order(node.right_child)
         end
     end
 
--- test/binary_tree/test_binary_tree.rb
+++ test/binary_tree/test_binary_tree.rb
@@ -13,11 +13,18 @@ class TestBinaryTree < Minitest::Test
         @fixtures = nil
     end    
 
-    def get_fixture(index = nil)
+    def get_fixture(index = nil, delete = true)
         fixture = (index) ? @fixtures[index] : @fixtures.sample
-        @fixtures.delete(fixture)
+        @fixtures.delete(fixture) if delete
 
         return fixture
+    end
+
+    def insert_fixtures
+        for key in 0..@fixtures.length() -1
+            fixture = get_fixture(key, false)
+            @tree.add_node(fixture['key'], fixture['value'])
+        end
     end
 
     def test_node_has_appropriate_left_right_properties
@@ -45,6 +52,26 @@ class TestBinaryTree < Minitest::Test
             assert_equal(second_fixture['key'], @tree.root.right_child.key)
         else
             assert_equal(second_fixture['key'], @tree.root.left_child.key)
+        end
+    end
+
+    def test_find_a_node_placed_in_the_tree
+        insert_fixtures
+
+        fixture = get_fixture
+        found_fixture = @tree.find(fixture['key'])
+
+        assert_equal(fixture['value'], found_fixture.value)
+    end
+
+    def test_node_traversed_in_order
+        insert_fixtures
+            
+        current = 0
+        
+        @tree.travese_in_order do |node|
+            assert(node.key > current)
+            current = node
         end
     end
 end
