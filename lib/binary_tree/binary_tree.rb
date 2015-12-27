b34506c2c256a3bfe76bb0af1f234c1b8b872dd9
hugorut <hugorut@gmail.com>
2015-12-27 16:32:36 +0000

    delete added to tree

--- lib/binary_tree/binary_tree.rb
+++ lib/binary_tree/binary_tree.rb
@@ -56,8 +56,12 @@ module BinaryTree
         # find a node in the tree using a index key
         def find(index)
             current = @root
+            parent = @root
 
             while current.key != index
+                # store a parent variable in memory so that we can access it for deletion
+                parent = current
+
                 # get the property to travese, either left_child or right_child
                 # then set current to equal that traversal
                 property = direction(index, current.key)
@@ -71,14 +75,72 @@ module BinaryTree
             end
 
             # we've found the node! return it
-            return current
+            if block_given?
+                yield parent, current, property
+            else
+                return current    
+            end
+        end
+
+        # delete a specific node using an index key to locate it
+        def delete_node(index)
+            # find the index of the node and call the block in order to acess the properties
+            # and parent required to keep the code DRY
+            find(index) do |parent, to_be_deleted, property|
+                if to_be_deleted.left_child.nil? and to_be_deleted.right_child.nil?
+                    # if both the nodes left and right child are nil then this means we can
+                    # simply remove the node without having to do any extra leg work
+                    set_replacement(parent, to_be_deleted, nil, property)
+                elsif to_be_deleted.right_child.nil?
+                    # if just the right child is nil then all we have to do is move up the left_child                
+                    replacement = to_be_deleted.left_child
+                    set_replacement(parent, to_be_deleted, replacement, property)      
+                elsif to_be_deleted.left_child.nil?
+                    # if jus the left child is nil then all we have to do is move up the right_child
+                    replacement = to_be_deleted.right_child
+                    set_replacement(parent, to_be_deleted, replacement, property) 
+                else
+                    # otherwise we need to locate the lowest left child of tree, we store it in a pointer 
+                    # in memory and then delete it from the tree by calling the method again
+                    replacement = min_left_child(to_be_deleted.right_child)
+                    delete_node(replacement.key)
+
+                    # then we assing the node to be deleted left and right child values
+                    replacement.left_child = to_be_deleted.left_child 
+                    replacement.right_child = to_be_deleted.right_child
+
+                    # once the replacement node has the correct values and has been removed from its previous
+                    # position we call the set replacement method on the node to be deleted parent in order
+                    # to remove it from refrence in the tree
+                    set_replacement(parent, to_be_deleted, replacement, property)
+                end
+            end
+        end
+
+        # set a replacement  by either sending a left_child or right_child to call
+        # a setter method on the parent node
+        def set_replacement(parent, node, replacement, property)
+            if node == @root
+                @root = replacement
+            else
+                  parent.send(property+'=',replacement)
+            end
+        end
+
+        # find the min left value of the left children
+        def min_left_child(node)
+            if node.left_child.nil?
+                return node
+            end
+
+            return min_left_child(node.left_child)
         end
 
         # traverse the tree in order of accending value and yield
         # a block with the node as a parameter if given
         def travese_in_order(node = @root)
             return if node == nil
-            
+
             travese_in_order(node.left_child)
             yield node if block_given?
             travese_in_order(node.right_child)
--- test/binary_tree/test_binary_tree.rb
+++ test/binary_tree/test_binary_tree.rb
@@ -68,10 +68,103 @@ class TestBinaryTree < Minitest::Test
         insert_fixtures
             
         current = 0
-        
+
         @tree.travese_in_order do |node|
             assert(node.key > current)
             current = node
         end
+    end
+
+    def test_delete_node_at_root
+        insert_fixtures
+
+        #    |50|
+        #   30 78
+        #  12    90
+        # 5 16
+        #  14 22
+
+        @tree.delete_node(50)
+
+        #    78
+        #   30 90
+        #  12 
+        # 5 16
+        #  14 22
+
+        node = @tree.find(78)
+        assert_equal(78, @tree.root.key)
+        assert_equal(30, node.left_child.key)        
+        assert_equal(90, node.right_child.key)        
+    end    
+
+    def test_delete_node_with_no_right_children
+        insert_fixtures
+
+        #     50
+        #   |30| 78
+        #  12      90
+        # 5 16
+        #  14 22
+
+        @tree.delete_node(30)
+
+        #    50
+        #  12  78
+        # 5 16   90
+        #  14 22
+
+        node = @tree.find(12)
+        assert_equal(12, @tree.root.left_child.key)   
+        assert_equal(16, node.right_child.key)        
+    end    
+
+    def test_delete_node_with_no_left_children
+        insert_fixtures
+
+        #     50
+        #    30 |78|
+        #  12     90
+        # 5 16
+        #  14 22
+
+        @tree.delete_node(78)
+
+        #     50
+        #    30 90
+        #  12     
+        # 5 16
+        #  14 22
+
+        node = @tree.find(90)
+        assert_equal(90, @tree.root.right_child.key)   
+    end
+
+    def test_delete_a_node_which_moves_min_value_in_place_
+        insert_fixtures
+
+        #    50
+        #   30 78
+        # |12|  90
+        # 5 16
+        #  14 22
+
+        @tree.delete_node(12)
+
+        #    50
+        #   30 78
+        #  14   90
+        # 5 16
+        #    22
+
+        node = @tree.find(14)
+        assert_equal(5, node.left_child.key)
+        assert_equal(16, node.right_child.key)
+
+        node = @tree.find(30)
+        assert_equal(14, node.left_child.key)
+
+        node = @tree.find(16)
+        assert_nil(node.left_child)
     end
 end
